a973292fb20dfd6f1eb25806ce00c78a990d3401
alkakaswan <alkakaswan67@gmail.com>
2025-08-30 08:00:19 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,35 @@
+// # primitive
+
+// 7 types : string ,Number, boolean, null , undefiend , Symbol ,BigInt
+
+const score = 100
+const scoreValue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(Id === anotherId);
+
+const bigNumber = 3259128429844888n
+
+
+
+// # reference (Non primitive)
+
+// array ,object , functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+
+name: "alka",
+age: 21,
+
+}
+
+ const  myFunction = function () {
+    console.log("hello word");
+ }
+  console.log(typeof bigNumber) // datatype pata karne k liye
